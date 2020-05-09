489ed0b738809386bd706fb72f1a1b9b02fda6e3
b18030 <tushar22.tg.tg@gmail.com>
2020-05-10 00:02:20 +0530

    Add Tag Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -7,7 +7,15 @@ class StudentSerializer(serializers.Serializer):
     marks = serializers.IntegerField()
 
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Tag
+        fields = '__all__'
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True,read_only=True)
     class Meta:
         model = models.Article
         fields = '__all__'
+
