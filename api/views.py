0fc12675050a9fc084a786b5f9dce335e4943826
b18030 <tushar22.tg.tg@gmail.com>
2020-05-09 23:11:28 +0530

    Add view for Create Article API

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-# import json
+import json
 
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
@@ -21,6 +21,18 @@ def articleApi(request):
     response = serializers.ArticleSerializer(articles,many=True)
     return Response(response.data)
 
+@api_view(['POST'])
+def createArticleApi(request):
+    # print(request.body)
+    body = json.loads(request.body)
+    response = serializers.ArticleSerializer(data=body)
+    # print(body)
+
+    if response.is_valid():
+        inst = response.save()
+        response = serializers.ArticleSerializer(inst)
+        return Response(response.data)
+    return Response(response.errors)
 
 @api_view()
 def usersApi(request):
